c8420dd1dfaf4608a5720abc137778624e2bdbe1
kaleb-huneau <krhuneau@gmail.com>
2022-11-15 00:16:09 -0500

    added tests, cleaned up comments

--- gcd.c
+++ gcd.c
@@ -3,23 +3,24 @@
 //
 #include <stdio.h>
 
-int gcd(int x, int y){//this function takes two integers and returns the GCD of both (recursion)
-    if(y == 0){//if one of the numbers is zero, return zero
-        return x;
-    }
-    else if(x < y){
-        return gcd(y,x);
-    }else{
-        return gcd(y, x % y);
-    }
+int gcd(int x, int y){
+    /*
+     * This function takes two integers and returns the greatest common denominator using recursion
+     */
+    if(y == 0){return x;}             //if y is zero, gcd must be zero
+    else if(x < y){return gcd(y,x);}  //recursive call if x < y
+    else{return gcd(y, x % y);}    //recursive call with remainder of x/y
 }
-
-
 int main(){
-    //test the gcd here:
+    //Testing the function
     int x = 54;
     int y = 24;
-    printf("GCD of x = %d, and y = %d is %d\n", x, y, gcd(x,y));
+    int result = gcd(x, y);
+
+    printf("GCD of x = %d, and y = %d is %d\n", x, y, result);
+
+    if(result == 6){printf("Gives correct result for 54 and 24")};
+    if(result == gcd(y, x)){printf("Confirmed that given order does not matter")};
 
     return 0;
 }
